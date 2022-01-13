cf78c13b0a167f8b6b31f1658471ebef14adacdc
Boluwa-tifey <boluwatifextianie@gmail.com>
2022-01-13 07:31:53 -0800

    bio-data app

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function register() {
+
+    var first = firstname.value;
+    var last = lastname.value;
+    var gen = gender.value;
+    var phone = phonenumber.value;
+    var email = email22.value;
+    var pass = password.value;
+    var level = level22.value;
+    var dept = department.value;
+
+    console.log(firstname.value)
+}
